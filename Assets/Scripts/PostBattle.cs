b6852f0580d056556bb5c717e16d6a801e207603
Akashi42 <95761894+akashis42@users.noreply.github.com>
2024-11-30 22:12:23 +0100

    add PostBattle

---
+++ Assets/Scripts/PostBattle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PostBattle : MonoBehaviour
+{
+
+    public void upgradeCard(){
+
+    }
+
+    public void swapCard(){
+        
+    }
+
+    public void HealInBetweenBattle(){
+        
+    }
+
+
+}
